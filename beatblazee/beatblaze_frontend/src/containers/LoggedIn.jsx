84d70d16c362c0779af136e686489b997f44e69c
devansh0627 <101563736+devansh0627@users.noreply.github.com>
2024-04-11 02:48:43 +0530

    Update LoggedIn.jsx

--- beatblazee/beatblaze_frontend/src/containers/LoggedIn.jsx
+++ beatblazee/beatblaze_frontend/src/containers/LoggedIn.jsx
@@ -9,6 +9,7 @@ import songContext from "../contexts/songContext.jsx";
 import CreatePlaylist from "../modals/CreatePlaylist.jsx";
 import AddToPlaylist from "../modals/AddToPlaylist.jsx";
 import { makeAuthenticatedPOSTRequest, makeAuthenticatedGETRequest } from "../utils/serveRoutes.jsx";
+import { useMediaQuery } from 'react-responsive';
 
 const LoggedIn = ({ children, currActive }) => {
   const [activeTab, setActiveTab] = useState(currActive);
@@ -79,7 +80,7 @@ const LoggedIn = ({ children, currActive }) => {
       // Stop the timer when the sound is stopped
       clearInterval(timerId);
     });
-    
+
     console.log(sound);
     setIsPaused(false);
     sound.play();
@@ -219,76 +220,85 @@ const LoggedIn = ({ children, currActive }) => {
       return '/images/repeat_none.svg';
     else return '/images/repeat_one.svg';
   }
-  const handleRepeat=()=>{
+  const handleRepeat = () => {
     if (repeat === 'all')
-    setRepeat('none');
-  else if (repeat === 'none')
-    setRepeat('one');
-  else setRepeat('all');
+      setRepeat('none');
+    else if (repeat === 'none')
+      setRepeat('one');
+    else setRepeat('all');
   }
   const val = useRef(shuffle);
 
-useEffect(() => {
+  useEffect(() => {
     console.log(repeat);
     val.current = shuffle;
     console.log(val.current);
 
     const handleSoundEnd = () => {
-        console.log('sound end');
-        if (repeat === 'none') {
-            setIsPaused(true);
-        } else if (repeat === 'one') {
-            const temp = currentSong;
-            setCurrentSong(null);
-            setTimeout(() => {
-                setCurrentSong(temp);
-            }, 100);
-        } else if (val.current === false) {
-            const currentIndex = currSongList.findIndex((item) => item.track === currentSong.track);
-            console.log('enter non shuffle' + currentIndex)
-            if (currentIndex < currSongList.length - 1) {
-                const nextsong = currSongList[currentIndex + 1];
-                setCurrentSong(nextsong);
-            } else if (currentIndex === currSongList.length - 1) {
-                setIsPaused(true);
-            }
-        } else if (val.current === true) {
-            console.log('enter shuffle')
-            let mini = 0, maxi = currSongList.length - 1;
-            let newIdx = currSongList.findIndex((item) => item.track === currentSong.track), currIdx = newIdx;
-            let cnt = 0; //just for hypothetical case that it's not able to find random number within our desired time complexity
-            while (newIdx == currIdx || newIdx == currIdx + 1) {
-                newIdx = Math.floor(Math.random() * (maxi - mini + 1) + mini),
-                    cnt++;
-                if (cnt == 1e5) {
-                    if (currSongList.length == 2) // for song length 2 
-                        newIdx = currIdx == 1 ? 0 : 1;
-                    else if (currIdx + 2 < currSongList.length)
-                        newIdx = currIdx + 2;
-                    else
-                        newIdx = 0;
-                    break;
-                }
-            }
-            const nextsong = currSongList[newIdx];
-            if(newIdx!=currIdx)
-            setCurrentSong(nextsong);
-            else{
-              const temp = currentSong;
-              setCurrentSong(null);
-              setTimeout(() => {
-                  setCurrentSong(temp);
-              }, 100);
-            }
+      console.log('sound end');
+      if (repeat === 'none') {
+        setIsPaused(true);
+      } else if (repeat === 'one') {
+        const temp = currentSong;
+        setCurrentSong(null);
+        setTimeout(() => {
+          setCurrentSong(temp);
+        }, 100);
+      } else if (val.current === false) {
+        const currentIndex = currSongList.findIndex((item) => item.track === currentSong.track);
+        console.log('enter non shuffle' + currentIndex)
+        if (currentIndex < currSongList.length - 1) {
+          const nextsong = currSongList[currentIndex + 1];
+          setCurrentSong(nextsong);
+        } else if (currentIndex === currSongList.length - 1) {
+          setIsPaused(true);
         }
+      } else if (val.current === true) {
+        console.log('enter shuffle')
+        let mini = 0, maxi = currSongList.length - 1;
+        let newIdx = currSongList.findIndex((item) => item.track === currentSong.track), currIdx = newIdx;
+        let cnt = 0; //just for hypothetical case that it's not able to find random number within our desired time complexity
+        while (newIdx == currIdx || newIdx == currIdx + 1) {
+          newIdx = Math.floor(Math.random() * (maxi - mini + 1) + mini),
+            cnt++;
+          if (cnt == 1e5) {
+            if (currSongList.length == 2) // for song length 2 
+              newIdx = currIdx == 1 ? 0 : 1;
+            else if (currIdx + 2 < currSongList.length)
+              newIdx = currIdx + 2;
+            else
+              newIdx = 0;
+            break;
+          }
+        }
+        const nextsong = currSongList[newIdx];
+        if (newIdx != currIdx)
+          setCurrentSong(nextsong);
+        else {
+          const temp = currentSong;
+          setCurrentSong(null);
+          setTimeout(() => {
+            setCurrentSong(temp);
+          }, 100);
+        }
+      }
     }
 
     soundPlayed?.on('end', handleSoundEnd);
 
     return () => {
-        soundPlayed?.off('end', handleSoundEnd);
+      soundPlayed?.off('end', handleSoundEnd);
     };
-}, [repeat, shuffle, soundPlayed]);
+  }, [repeat, shuffle, soundPlayed]);
+
+  // responsiveness adjustments
+  const openingLeftPane = () => {
+    document.querySelector(".leftPane").style.left = "0";
+  }
+  const closingLeftPane = () => {
+    document.querySelector(".leftPane").style.left = "-200%";
+  }
+  const isDesktopOrLaptop = useMediaQuery({ minDeviceWidth: 768 });
 
   return (
     <>
@@ -309,7 +319,10 @@ useEffect(() => {
           />
         )}
         <div className="w-screen h-screen flex" style={{ background: 'linear-gradient(to bottom, #333333, #000000)', color: "#ECF0F1" }}>
-          <div className="leftPane h-full w-1/5">
+          <div className="leftPane absolute left-[-200%] z-10 transition-all duration-300 ease-in-out w-[300px] lg:static lg:w-1/5 lg:h-full">
+            <div className="close absolute top-4 right-4 block cursor-pointer lg:hidden" onClick={closingLeftPane}>
+              <img src="/images/close.svg" className='invert' alt="" />
+            </div>
             <div className="home border-adjust mx-2 mt-2 bg-gray-950" style={{ borderRadius: '3%', padding: '7px', boxShadow: '0 0 10px  rgba(73, 71, 71, 0.7)', border: '1px solid rgba(255, 255, 255, 0.3)' }}>
               <div className="brand flex items-center gap-2">
                 <span><img className="logo w-12 h-12 rounded-lg bg-white aspect-w-1 aspect-h-1" src="/images/01 logo2.jpg" alt="BeatBlaze" /></span>
@@ -375,11 +388,11 @@ useEffect(() => {
               </ul>
             </div>
           </div>
-          <div className="rightPane h-full w-4/5">
+          <div className="rightPane h-full w-full lg:w-4/5">
             <div className="header border-adjust  p-2 flex justify-between rounded-lg">
               <div className="nav mt-2">
-                <div className="hamburgerContainer flex items-center justify-center">
-                  <img className="invert hamburger hidden h-40 w-40 cursor-pointer" src="/images/hamburger.svg" alt="" />
+                <div className="hamburgerContainer flex items-center justify-center gap-3">
+                  <img className="invert hamburger block h-10 w-10 cursor-pointer lg:hidden" src="/images/hamburger.svg" alt="" onClick={openingLeftPane} />
                   <div className="iteratorIcon flex items-center justify-center gap-3">
                     <img className="cursor-pointer" src="/images/arrowLeft.svg" alt="Go backward" />
                     <img className="cursor-pointer" src="/images/arrowRight.svg" alt="Go Forward" />
@@ -394,7 +407,7 @@ useEffect(() => {
             {children}
           </div>
         </div>
-        {currentSong && (
+        {isDesktopOrLaptop ? (currentSong && (
           <div className="playbar bg-gray-950" style={{ boxShadow: '0 0 10px  rgba(73, 71, 71, 0.7)', border: '1px solid rgba(255, 255, 255, 0.3)' }}>
             <div className="seekbar" style={{ background: `linear-gradient(to right, white ${circlePosition}%, grey ${circlePosition}%)` }} onClick={handleSeek}>
               <div className="circle" style={{ left: `${circlePosition}%` }}></div>
@@ -403,10 +416,10 @@ useEffect(() => {
               <div className="songInfo w-1/4 flex items-center left-part gap-3">
                 <img
                   src={currentSong.thumbnail}
-                  className="h-14 w-14 rounded"
+                  className="h-14 w-14 rounded playbarThumbnail"
                   alt="currentSongThumbnail"
                 />
-                <div>
+                <div className="songAndArtistName">
                   <div className="cursor-pointer hover:underline" style={{ color: "white", maxWidth: "100%", overflow: "hidden", textOverflow: "ellipsis", whiteSpace: "nowrap" }}>
                     {currentSong.name}
                   </div>
@@ -425,7 +438,7 @@ useEffect(() => {
                   togglePlayPuase();
                 }} />
                 <img id="next" src="/images/nextsong.svg" alt="next" onClick={nextSong} />
-                <img id="autoPlay" src={repeatIcon()} alt="repeat_all" onClick={()=>{handleRepeat();}}/>
+                <img id="autoPlay" src={repeatIcon()} alt="repeat_all" onClick={() => { handleRepeat(); }} />
                 <div className="volumeBar">
                   <span><img className="invert cursor-pointer relative top-1" id="volumeIcon" src={volumeIcon} alt="" onClick={() => {
                     if (volume === 0) {
@@ -475,12 +488,12 @@ useEffect(() => {
               <div className="songTime">
                 {formatDuration(currSongDuration)} / {formatDuration(songDuration)}
                 <div className="likeAndAddPlaylist flex gap-10">
-                  <img src="/images/likedSongs.svg" className={`w-7 h-7 relative top-3 cursor-pointer ${checkLikeOrNot ? "" : "invert"}`} alt="" onClick={() => {
+                  <img src="/images/likedSongs.svg" className={`w-7 h-7 relative top-3 heart cursor-pointer ${checkLikeOrNot ? "" : "invert"}`} alt="" onClick={() => {
                     if (checkLikeOrNot) { removeLikedSongs(); setCheckLikeOrNot(false); }
                     else { addSongToLikedSongs(); setCheckLikeOrNot(true); }
                   }} />
 
-                  <img src="/images/addPlaylist.svg" className='invert relative top-3 cursor-pointer' alt="" onClick={() => {
+                  <img src="/images/addPlaylist.svg" className='invert relative top-3 cursor-pointer addToPlaylist' alt="" onClick={() => {
                     setAddToPlaylistModalOpen(true);
                   }
                   } />
@@ -489,7 +502,95 @@ useEffect(() => {
             </div>
           </div>
 
-        )}
+        )) : (currentSong && (<div className="playbar bg-gray-950 flex flex-col text-white relative" style={{ boxShadow: '0 0 10px rgba(73, 71, 71, 0.7)', border: '1px solid rgba(255, 255, 255, 0.3)', height: '35%' }}>
+          <div className="flex justify-between px-3 absolute bottom-5 w-full">
+            <div>{formatDuration(currSongDuration)}</div>
+            <div>{formatDuration(songDuration)}</div>
+          </div>
+          <div className="seekbar" style={{ background: `linear-gradient(to right, white ${circlePosition}%, grey ${circlePosition}%)`, width: '95%' }} onClick={handleSeek}>
+            <div className="circle" style={{ left: `${circlePosition}%` }}></div>
+          </div>
+          <div className="songInfo absolute top-3 left-1/2 transform -translate-x-1/2 flex flex-col items-center">
+            <img
+              src={currentSong.thumbnail}
+              className="h-24 w-24 rounded playbarThumbnail"
+              alt="currentSongThumbnail"
+            />
+            <marquee behavior="scroll" direction="left" scrollamount="3" className="mt-2" style={{ color: "white", maxWidth: "100%", overflow: "hidden", whiteSpace: "nowrap" }}>
+              {currentSong.name}
+            </marquee>
+            <div style={{ color: "#c0c2c2", maxWidth: "100%", overflow: "hidden", whiteSpace: "nowrap" }}>
+              {currentSong.artist.firstName + " " + currentSong.artist.lastName}
+            </div>
+          </div>
+          <div className="songButtons absolute left-1/2 transform -translate-x-1/2 z-10" style={{ top: '63%' }}>
+            <img id="shuffle" src={shuffle ? "/images/shuffle.svg" : "/images/shuffle2.svg"} alt="shuffle" onClick={handleShuffle} />
+            <img id="prev" src="/images/prevsong.svg" alt="prev" onClick={prevSong} />
+            <img id="playy" src={isPaused ? "/images/play_button_bottom.svg" : "/images/pause.svg"} alt="play" onClick={() => {
+              togglePlayPuase();
+            }} />
+            <img id="next" src="/images/nextsong.svg" alt="next" onClick={nextSong} />
+            <img id="autoPlay" src={repeatIcon()} alt="repeat_all" onClick={() => { handleRepeat(); }} />
+            <div className="volumeBar">
+              <span><img className="invert cursor-pointer relative top-1" id="volumeIcon" src={volumeIcon} alt="" onClick={() => {
+                if (volume === 0) {
+                  setVolume(originalVol.current);
+                  soundPlayed.volume(originalVol.current);
+                  const per = Math.floor((originalVol.current - 0) / (1 - 0) * 100);
+                  if (originalVol.current == 0) {
+                    setVolumeIcon('/images/volume_mute.svg');
+                  }
+                  else if (per < 30) {
+                    setVolumeIcon('/images/volume_low.svg');
+                  } else if (per >= 30 && per <= 60) {
+                    setVolumeIcon('/images/volume_medium.svg');
+                  } else {
+                    setVolumeIcon('/images/volume_high.svg');
+                  }
+                }
+                else {
+                  originalVol.current = volume;
+                  setVolume(0);
+                  soundPlayed.volume(0);
+                  setVolumeIcon('/images/volume_mute.svg');
+                }
+              }
+              } /></span>
+              <span><input className="volume-slider" type="range" min="0" max="1" value={volume} step="0.01" onInput={(e) => {
+                const newVolume = parseFloat(e.target.value);
+                const per = Math.floor((newVolume - 0) / (1 - 0) * 100);
+                if (newVolume == 0) {
+                  setVolumeIcon('/images/volume_mute.svg');
+                }
+                else if (per < 30) {
+                  setVolumeIcon('/images/volume_low.svg');
+                } else if (per >= 30 && per <= 60) {
+                  setVolumeIcon('/images/volume_medium.svg');
+                } else {
+                  setVolumeIcon('/images/volume_high.svg');
+                }
+                setVolume(newVolume);
+                soundPlayed.volume(newVolume);
+              }} style={{
+                background: `linear-gradient(to right, white ${volumePercent}%, grey ${volumePercent}%)`
+              }} /></span>
+              <span className="volume-level relative top-1">{Math.floor(volumePercent)}</span>
+            </div>
+          </div>
+          <div className="likeAndAddPlaylist flex justify-around gap-14 relative" style={{top:'59%'}}>
+            <img src="/images/likedSongs.svg" className={`w-7 h-7 relative top-3 heart cursor-pointer ${checkLikeOrNot ? "" : "invert"}`} alt="" onClick={() => {
+              if (checkLikeOrNot) { removeLikedSongs(); setCheckLikeOrNot(false); }
+              else { addSongToLikedSongs(); setCheckLikeOrNot(true); }
+            }} />
+
+            <img src="/images/addPlaylist.svg" className='invert relative top-3 cursor-pointer addToPlaylist' alt="" onClick={() => {
+              setAddToPlaylistModalOpen(true);
+            }
+            } />
+          </div>
+        </div>
+
+        ))}
 
       </div>
     </>
