568486f38d8a976e5ee3d7ade706893c18f20f0c
devansh0627 <101563736+devansh0627@users.noreply.github.com>
2024-04-11 02:50:23 +0530

    Update Home.jsx

--- beatblazee/beatblaze_frontend/src/routes/Home.jsx
+++ beatblazee/beatblaze_frontend/src/routes/Home.jsx
@@ -29,18 +29,28 @@ const Home = () => {
   //   return true;
   // }
   // the above commented part is part of infinite rendering.The error also occurs if we try to set a component's state immediately, without using a condition or an event handler.
-  const [loginAndSignUpOverlay,setLoginAndSignUpOverlay]=useState(false);
+
+  const openingLeftPane = () => {
+    document.querySelector(".leftPane").style.left = "0";
+  }
+  const closingLeftPane = () => {
+    document.querySelector(".leftPane").style.left = "-200%";
+  }
+  const [loginAndSignUpOverlay, setLoginAndSignUpOverlay] = useState(false);
   return (
     <>
       <div className="w-screen h-screen flex" style={{ background: "linear-gradient(to bottom, #333333, #000000)", color: '#ECF0F1' }}>
-      {loginAndSignUpOverlay && (
+        {loginAndSignUpOverlay && (
           <LoginAndSignUpOverlay
-          closeModal={() => {
-            setLoginAndSignUpOverlay(false);
-          }}
-        />
+            closeModal={() => {
+              setLoginAndSignUpOverlay(false);
+            }}
+          />
         )}
-        <div className="leftPane h-full w-1/5">
+        <div className="leftPane absolute left-[-200%] z-10 transition-all duration-300 ease-in-out w-[300px] lg:static lg:w-1/5 lg:h-full">
+          <div className="close absolute top-4 right-4 block cursor-pointer lg:hidden" onClick={closingLeftPane}>
+            <img src="/images/close.svg" className='invert' alt="" />
+          </div>
           <div className="home border-adjust mx-2 mt-2 bg-gray-950" style={{ borderRadius: '3%', padding: '7px', boxShadow: '0 0 10px  rgba(73, 71, 71, 0.7)', border: '1px solid rgba(255, 255, 255, 0.3)' }}>
             <div className="brand flex items-center gap-2">
               <span><img className="logo w-12 h-12 rounded-lg bg-white aspect-w-1 aspect-h-1" src="/images/01 logo2.jpg" alt="BeatBlaze" /></span>
@@ -58,7 +68,7 @@ const Home = () => {
               </li>
               <li
                 className={`w-auto flex gap-3 items-center cursor-pointer py-4 ${activeTab === 'search' ? 'text-white' : hoverTab === 'search' ? 'text-white' : 'text-gray-400'}`}
-                onClick={() => { handleTabClick('search'); setLoginAndSignUpOverlay(true);}}
+                onClick={() => { handleTabClick('search'); setLoginAndSignUpOverlay(true); }}
                 onMouseEnter={() => handleTabHover('search')}
                 onMouseLeave={() => handleTabHover(null)}
                 style={{ transition: 'color 0.3s ease' }}
@@ -67,7 +77,7 @@ const Home = () => {
               </li>
               <li
                 className={`w-auto flex gap-3 items-center cursor-pointer py-4 ${activeTab === 'library' ? 'text-white' : hoverTab === 'library' ? 'text-white' : 'text-gray-400'}`}
-                onClick={() => {handleTabClick('library');setLoginAndSignUpOverlay(true);}}
+                onClick={() => { handleTabClick('library'); setLoginAndSignUpOverlay(true); }}
                 onMouseEnter={() => handleTabHover('library')}
                 onMouseLeave={() => handleTabHover(null)}
                 style={{ transition: 'color 0.3s ease' }}
@@ -78,7 +88,7 @@ const Home = () => {
             <ul className="pt-5 m-1.5">
               <li
                 className={`w-auto flex gap-3 items-center cursor-pointer py-4 ${activeTab === 'createPlaylist' ? 'text-white' : hoverTab === 'createPlaylist' ? 'text-white' : 'text-gray-400'}`}
-                onClick={() => {handleTabClick('createPlaylist');setLoginAndSignUpOverlay(true);}}
+                onClick={() => { handleTabClick('createPlaylist'); setLoginAndSignUpOverlay(true); }}
                 onMouseEnter={() => handleTabHover('createPlaylist')}
                 onMouseLeave={() => handleTabHover(null)}
                 style={{ transition: 'color 0.3s ease' }}
@@ -87,7 +97,7 @@ const Home = () => {
               </li>
               <li
                 className={`w-auto flex gap-3 items-center cursor-pointer py-4 ${activeTab === 'likedSongs' ? 'text-white' : hoverTab === 'likedSongs' ? 'text-white' : 'text-gray-400'}`}
-                onClick={() => {handleTabClick('likedSongs');setLoginAndSignUpOverlay(true);}}
+                onClick={() => { handleTabClick('likedSongs'); setLoginAndSignUpOverlay(true); }}
                 onMouseEnter={() => handleTabHover('likedSongs')}
                 onMouseLeave={() => handleTabHover(null)}
                 style={{ transition: 'color 0.3s ease' }}
@@ -97,11 +107,11 @@ const Home = () => {
             </ul>
           </div>
         </div>
-        <div className="rightPane h-full w-4/5">
+        <div className="rightPane h-full w-full lg:w-4/5">
           <div className="header border-adjust  p-2 flex justify-between rounded-lg">
             <div className="nav mt-2">
-              <div className="hamburgerContainer flex items-center justify-center">
-                <img className="invert hamburger hidden h-40 w-40 cursor-pointer" src="/images/hamburger.svg" alt="" />
+              <div className="hamburgerContainer flex items-center justify-center gap-3">
+                <img className="invert hamburger block h-10 w-10 cursor-pointer lg:hidden" src="/images/hamburger.svg" alt="" onClick={openingLeftPane} />
                 <div className="iteratorIcon flex items-center justify-center gap-3">
                   <img className="cursor-pointer" src="/images/arrowLeft.svg" alt="Go backward" />
                   <img className="cursor-pointer" src="/images/arrowRight.svg" alt="Go Forward" />
@@ -112,7 +122,7 @@ const Home = () => {
 
               <button
                 className="signupbtn py-2 px-4 text-lg rounded-3xl bg-gray-900 hover:scale-105 cursor-pointer"
-                style={{color: "#ECF0F1", transition: 'all 0.3s ease' }}
+                style={{ color: "#ECF0F1", transition: 'all 0.3s ease' }}
                 onClick={() => navigate('/signup')}
               >
                 Sign up
@@ -130,66 +140,67 @@ const Home = () => {
           </div>
           <div className="beatblazePlaylists p-4">
             <h1 className="text-2xl">BeatBlaze Playlists</h1>
-            <div className="cardContainer m-8 flex gap-2.5 flex-wrap overflow-y-auto" style={{ maxHeight: '65vh' }}>
+            <div className="cardContainer mt-8 overflow-y-auto grid gap-x-2 grid-flow-col-dense sm:grid-flow-row-dense sm:grid sm: gap-y-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-5" style={{ maxHeight: '65vh' }}>
+
               <Card
                 source1="/images/play_button.svg" source2="/images/hip_hop.png" name="Hip Hop" desc="Dive into raw hip-hop vibes – where beats speak and rhymes tell stories.
-                  🎤🔥"></Card>
+            🎤🔥"></Card>
               <Card
                 source1="/images/play_button.svg" source2="/images/jazz (1).png" name="Jazz" desc="Explore soulful notes, where melodies tell tales, and improvisation
-                weaves musical magic. 🎵✨"></Card>
+            weaves musical magic. 🎵✨"></Card>
               <Card
                 source1="/images/play_button.svg" source2="/images/phonk.png" name="Phonk" desc="Unleashing aggressive beats and gritty lo-fi vibes in a raw, atmospheric sonic journey.😈🔥"></Card>
               <Card
                 source1="/images/play_button.svg" source2="/images/hip_hop.png" name="Hip Hop" desc="Dive into raw hip-hop vibes – where beats speak and rhymes tell stories.
-                  🎤🔥"></Card>
+            🎤🔥"></Card>
               <Card
                 source1="/images/play_button.svg" source2="/images/jazz (1).png" name="Jazz" desc="Explore soulful notes, where melodies tell tales, and improvisation
-                weaves musical magic. 🎵✨"></Card>
+            weaves musical magic. 🎵✨"></Card>
               <Card
                 source1="/images/play_button.svg" source2="/images/phonk.png" name="Phonk" desc="Unleashing aggressive beats and gritty lo-fi vibes in a raw, atmospheric sonic journey.😈🔥"></Card>
               <Card
                 source1="/images/play_button.svg" source2="/images/hip_hop.png" name="Hip Hop" desc="Dive into raw hip-hop vibes – where beats speak and rhymes tell stories.
-                  🎤🔥"></Card>
+            🎤🔥"></Card>
               <Card
                 source1="/images/play_button.svg" source2="/images/jazz (1).png" name="Jazz" desc="Explore soulful notes, where melodies tell tales, and improvisation
-                weaves musical magic. 🎵✨"></Card>
+            weaves musical magic. 🎵✨"></Card>
               <Card
                 source1="/images/play_button.svg" source2="/images/phonk.png" name="Phonk" desc="Unleashing aggressive beats and gritty lo-fi vibes in a raw, atmospheric sonic journey.😈🔥"></Card>
               <Card
                 source1="/images/play_button.svg" source2="/images/hip_hop.png" name="Hip Hop" desc="Dive into raw hip-hop vibes – where beats speak and rhymes tell stories.
-                  🎤🔥"></Card>
+                🎤🔥"></Card>
               <Card
                 source1="/images/play_button.svg" source2="/images/jazz (1).png" name="Jazz" desc="Explore soulful notes, where melodies tell tales, and improvisation
-                weaves musical magic. 🎵✨"></Card>
+            weaves musical magic. 🎵✨"></Card>
               <Card
                 source1="/images/play_button.svg" source2="/images/phonk.png" name="Phonk" desc="Unleashing aggressive beats and gritty lo-fi vibes in a raw, atmospheric sonic journey.😈🔥"></Card>
               <Card
                 source1="/images/play_button.svg" source2="/images/hip_hop.png" name="Hip Hop" desc="Dive into raw hip-hop vibes – where beats speak and rhymes tell stories.
-                  🎤🔥"></Card>
+            🎤🔥"></Card>
               <Card
                 source1="/images/play_button.svg" source2="/images/jazz (1).png" name="Jazz" desc="Explore soulful notes, where melodies tell tales, and improvisation
-                weaves musical magic. 🎵✨"></Card>
+            weaves musical magic. 🎵✨"></Card>
               <Card
                 source1="/images/play_button.svg" source2="/images/phonk.png" name="Phonk" desc="Unleashing aggressive beats and gritty lo-fi vibes in a raw, atmospheric sonic journey.😈🔥"></Card>
               <Card
                 source1="/images/play_button.svg" source2="/images/hip_hop.png" name="Hip Hop" desc="Dive into raw hip-hop vibes – where beats speak and rhymes tell stories.
-                  🎤🔥"></Card>
+            🎤🔥"></Card>
               <Card
                 source1="/images/play_button.svg" source2="/images/jazz (1).png" name="Jazz" desc="Explore soulful notes, where melodies tell tales, and improvisation
-                weaves musical magic. 🎵✨"></Card>
+            weaves musical magic. 🎵✨"></Card>
               <Card
                 source1="/images/play_button.svg" source2="/images/phonk.png" name="Phonk" desc="Unleashing aggressive beats and gritty lo-fi vibes in a raw, atmospheric sonic journey.😈🔥"></Card>
               <Card
                 source1="/images/play_button.svg" source2="/images/hip_hop.png" name="Hip Hop" desc="Dive into raw hip-hop vibes – where beats speak and rhymes tell stories.
-                  🎤🔥"></Card>
+            🎤🔥"></Card>
               <Card
                 source1="/images/play_button.svg" source2="/images/jazz (1).png" name="Jazz" desc="Explore soulful notes, where melodies tell tales, and improvisation
-                weaves musical magic. 🎵✨"></Card>
+            weaves musical magic. 🎵✨"></Card>
               <Card
                 source1="/images/play_button.svg" source2="/images/phonk.png" name="Phonk" desc="Unleashing aggressive beats and gritty lo-fi vibes in a raw, atmospheric sonic journey.😈🔥"></Card>
               <Card
                 source1="/images/play_button.svg" source2="/images/hip_hop.png" name="Hip Hop" desc="Dive into raw hip-hop vibes – where beats speak and rhymes tell stories.
-                  🎤🔥"></Card>
+            🎤🔥"></Card>
               <Card
                 source1="/images/play_button.svg" source2="/images/jazz (1).png" name="Jazz" desc="Explore soulful notes, where melodies tell tales, and improvisation
                 weaves musical magic. 🎵✨"></Card>
