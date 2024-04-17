f83cbc956685cf337f19250a726fcd0d8e06f5c6
devansh0627 <101563736+devansh0627@users.noreply.github.com>
2024-04-17 20:22:41 +0530

    Update SignUp.jsx

--- beatblazee/beatblaze_frontend/src/routes/SignUp.jsx
+++ beatblazee/beatblaze_frontend/src/routes/SignUp.jsx
@@ -6,6 +6,7 @@ import * as utils from '../utils/serveRoutes.jsx'
 import { useCookies } from 'react-cookie';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useMediaQuery } from 'react-responsive';
 const SignUp = () => {
     const [email, setEmail] = useState('');
     const [confirmEmail, setConfirmEmail] = useState('');
@@ -72,11 +73,11 @@ const SignUp = () => {
             });
         }
     }
-
+    const isDesktopOrLaptop = useMediaQuery({ minDeviceWidth: 768 });
     return (
         <>
             <ToastContainer position="top-center" autoClose={1500} pauseOnHover={false} />
-            <div className="flex flex-col items-center text-white" style={{ background: "linear-gradient(to bottom, #333333, #000000)" }}>
+            {isDesktopOrLaptop ? (<div className="flex flex-col items-center text-white" style={{ background: "linear-gradient(to bottom, #333333, #000000)" }}>
                 <div className="brand flex items-center gap-2 p-5 border-b w-full justify-center border-solid">
                     <span><img className="w-12 h-12 rounded-lg bg-white border border-black" src="/images/01 logo2.jpg" alt="BeatBlaze" /></span>
                     <span className="brand-name text-2xl font-bold">BeatBlaze</span>
@@ -163,7 +164,96 @@ const SignUp = () => {
                         </Link>
                     </div>
                 </div>
-            </div>
+            </div>) : (<div className="flex flex-col items-center text-white" style={{ background: "linear-gradient(to bottom, #333333, #000000)" }}>
+                <div className="brand flex items-center gap-2 p-5 border-b w-full justify-center border-solid">
+                    <span><img className="w-12 h-12 rounded-lg bg-white border border-black" src="/images/01 logo2.jpg" alt="BeatBlaze" /></span>
+                    <span className="brand-name text-2xl font-bold">BeatBlaze</span>
+                </div>
+                <div className="inputRegion w-full py-10 flex items-center justify-center flex-col">
+                    <div className="font-bold mb-4 text-2xl text-center text-pretty">
+                        Sign up and ignite your musical journey with BeatBlaze!
+                    </div>
+                    <div className='w-full px-2 flex flex-col pb-5'>
+                        <TextInput
+                            label="What's your email?"
+                            placeholder="Enter your email."
+                            className="my-6"
+                            value={email}
+                            setValue={handleEmailChange}
+                        />
+                        {emailError && <div className="flex items-center text-red-500 text-sm"><img className='h-5 w-5' src="/images/error.svg" alt="error" />{emailError}</div>}
+                    </div>
+                    <div className="w-full flex px-2 flex-col pb-5">
+                        <TextInput
+                            label="Confirm your email"
+                            placeholder="Enter your email again."
+                            className="mb-6"
+                            value={confirmEmail}
+                            setValue={handleConfirmEmailChange}
+                        />
+                        {confirmEmailError && <div className="flex items-center text-red-500 text-sm"><img className='h-5 w-5' src="/images/error.svg" alt="error" />{confirmEmailError}</div>}
+                    </div>
+                    <div className="w-full flex px-2 flex-col pb-5">
+                        <PasswordInput
+                            label="Create a password"
+                            placeholder="Create a password"
+                            value={password}
+                            setValue={setPassword}
+                        />
+                    </div>
+                    <div className='flex w-full px-2 justify-between items-center flex-wrap gap-2'>
+                        <TextInput
+                            label="First Name"
+                            placeholder="Enter your First Name"
+                            className="my-6"
+                            value={firstName}
+                            setValue={setFirstName}
+                        />
+                        <TextInput
+                            label="Last Name"
+                            placeholder="Enter your Last Name"
+                            className="my-6"
+                            value={lastName}
+                            setValue={setLastName}
+                        />
+                    </div>
+                    <div className="w-full flex px-2 flex-col pb-5">
+                        <TextInput
+                            label="Choose your username"
+                            placeholder="Enter your username"
+                            className="my-6"
+                            value={userName}
+                            setValue={setUserName}
+                        />
+                        {userNameError && <div className="flex items-center text-red-500 text-sm"><img className='h-5 w-5' src="/images/error.svg" alt="error" />{userNameError}</div>}
+                    </div>
+                    <div className=" w-full flex items-center justify-center my-8">
+                        <button
+                            className="font-semibold p-3 px-10 rounded-full transition-transform transform-gpu hover:scale-105"
+                            style={{ backgroundColor: "#008080", color: "white" }}
+                            onClick={(e) => {
+                                e.preventDefault();// prevent default functioning if there to get called 
+                                signUpDetails();
+                            }}
+                        >
+                            SIGN UP
+                        </button>
+                    </div>
+                    <div className="w-full border border-solid border-gray-300"></div>
+                    <div className="my-6 font-semibold text-lg">
+                        Already have an account?
+                    </div>
+                    <div className="border border-gray-500 text-gray-500 w-full flex items-center justify-center py-4 rounded-full font-bold">
+                        <Link
+                            to="/login"
+                            className="transition-transform transform-gpu hover:scale-105"
+                            style={{ color: '#00cccc' }}
+                        >
+                            LOGIN IN INSTEAD
+                        </Link>
+                    </div>
+                </div>
+            </div>)}
         </>
     );
 };
